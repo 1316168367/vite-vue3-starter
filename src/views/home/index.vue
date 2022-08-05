b91a4c14bfa567e1d0aa6e5b6121677ce5f14dc5
zengbowei <1316168367@qq.com>
2022-08-05 14:44:08 +0800

    使用nav

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,26 +1,35 @@
 <template>
-  <div>111</div>
-  <Button @click="aa">1111</Button>
+  <div class="home">
+    <Nav :title="title"></Nav>
+    <div>111</div>
+    <Button @click="aa">1111</Button>
+  </div>
 </template>
 
 <script lang="ts" setup>
 const $Message: any = inject('$Message')
 const $Spin: any = inject('$Spin')
 const $Modal: any = inject('$Modal')
+const title = ref<string>('')
 onMounted(() => {})
 const aa = () => {
   console.log($Message)
+  title.value = '首页'
   $Message.error('111')
   $Spin.show()
   setTimeout(() => {
     $Spin.hide()
     $Modal.info({
-      title:'1111',
-      content:'33333'
+      title: '1111',
+      content: '33333',
     })
-  }, 1000);
+  }, 1000)
   // globalProperties?.$Message.error('111')
 }
 </script>
 
-<style></style>
+<style lang="scss">
+.home {
+  margin-top: 55px;
+}
+</style>
